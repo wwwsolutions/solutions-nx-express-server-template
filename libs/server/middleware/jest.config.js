fd30627cb669a7190f84f4a838d9983262a3b325
Domagoj-Mario Mendas <42439126+wwwsolutions@users.noreply.github.com>
2022-04-24 22:38:56 +0200

    npm run nx -- migrate --run-migrations

--- libs/server/middleware/jest.config.js
+++ libs/server/middleware/jest.config.js
@@ -2,9 +2,7 @@ module.exports = {
   displayName: 'server-middleware',
   preset: '../../../jest.preset.js',
   globals: {
-    'ts-jest': {
-      tsConfig: '<rootDir>/tsconfig.spec.json',
-    },
+    'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' },
   },
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest',
